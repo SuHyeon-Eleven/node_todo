49e58a8614b80597e3ac4f3773df43afe4ec2724
suhyeon <minsoohyeon1234@gmail.com>
2021-11-23 10:44:34 +0900

    post up/down, 수정, delete 구현

--- app1.js
+++ app1.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const Todo = require("./models/todo")
-mongoose.connect(" mongodb://127.0.0.1:27017/todo-demo", {
+mongoose.connect("mongodb://localhost/todo-demo", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
@@ -10,16 +10,38 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 const app = express();
 const router = express.Router();
-
 router.get("/", (req, res) => {
     res.send("Hi!");
 });
-
-router.get("/todos", async (req, res)=>{
+router.get("/todos", async (req, res) => {
     const todos = await Todo.find().sort("-order").exec();
     res.send({ todos });
 });
-
+router.patch("/todos/:todoId", async (req, res) => {
+    const { todoId } = req.params;
+    const { order, value, done } = req.body;
+    const todo = await Todo.findById(todoId).exec();
+    if (order) {
+        const targetTodo = await Todo.findOne({ order }).exec();
+        if (targetTodo) {
+            targetTodo.order = todo.order;
+            await targetTodo.save();
+        }
+        todo.order = order;
+    } else if (value) {
+        todo.value = value;
+    } else if (done !== undefined) {
+        todo.doneAt = done ? new Date() : null;
+    }
+    await todo.save();
+    res.send({});
+});
+router.delete("/todos/todoId", async (req, res) => {
+    const { todoId } = req.params;
+    const todo = await Todo.findById(todoId).exec();
+    await todo.delte();
+    res.send({});
+})
 router.post("/todos", async (req, res) => {
     const { value } = req.body; //구조 분해 할당
     const maxOrderTodo = await Todo.findOne().sort("-order").exec(); //exec가 promise값이기 때문에 await로 동작 대기
@@ -29,41 +51,8 @@ router.post("/todos", async (req, res) => {
     }
     const todo = new Todo({ value, order });
     await todo.save();
-
     res.send({ todo });
 });
-
-router.patch("/todos/:todoId", async (req, res)=>{
-    const { todoId } = req.params;
-    const { order, value, done } = req.body;
-    
-    const todo = await Todo.findById(todoId).exec();
-    if (order) {
-        const targetTodo = await Todo.findOne({ order }).exec();
-        if (targetTodo){
-            targetTodo.order = todo.order;
-            await targetTodo.save();
-        }
-        todo.order = order;
-    }else if(value){
-        todo.value = value;
-        
-    }else if(done !== undefined){
-        todo.doneAt = done ? new Date() : null;
-    }
-    await todo.save();
-    res.send({}); 
-});
-
-router.delete("/todos/:todoId", async(req, res)=>{
-    const { todoId } =req.params;
-    // console.log({todoId});
-    const todo = await Todo.findById(todoId).exec();
-    todo.delete();
-
-    res.send({});
-})
-
 app.use("/api", bodyParser.json(), router);
 app.use(express.static("./assets"));
 app.listen(8080, () => {
